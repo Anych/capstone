8d0f2352e2931ddb8e64ea6d4d2e9a5d8e2386c2
Anych <anuar123@mail.ru>
2022-11-04 00:38:45 +0600

    Add MilaIrisBackend module

---
+++ MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/anuar/MilaIrisBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.anuar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilaIrisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilaIrisBackendApplication.class, args);
+    }
+
+}
