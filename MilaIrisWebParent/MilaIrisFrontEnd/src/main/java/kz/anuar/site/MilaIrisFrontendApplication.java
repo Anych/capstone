2c25282f9ce0eb62fc3fd970e0871379b250da48
Anych <anuar123@mail.ru>
2022-11-04 03:53:39 +0600

    Add MilaIriFrontend module

--- MilaIrisWebParent/MilaIrisFrontEnd/src/main/java/kz/anuar/site/MilaIrisFrontendApplication.java
+++ MilaIrisWebParent/MilaIrisFrontEnd/src/main/java/kz/anuar/site/MilaIrisFrontendApplication.java
@@ -1,12 +1,12 @@
-package kz.anuar;
+package kz.anuar.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MilaIrisBackendApplication {
+public class MilaIrisFrontendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MilaIrisBackendApplication.class, args);
+        SpringApplication.run(MilaIrisFrontendApplication.class, args);
     }
 }
