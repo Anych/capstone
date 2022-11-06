7b6db95e088df8f154a736cf66af3fe2879d4f38
Anych <anuar123@mail.ru>
2022-11-07 00:24:20 +0600

    Add delete user logic and change enable status

--- MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/user/UserRepository.java
+++ MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/user/UserRepository.java
@@ -1,6 +1,7 @@
 package kz.milairis.admin.user;
 
 import kz.milairis.common.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,4 +10,10 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     @Query("SELECT u from User u WHERE u.email = :email")
     public User getUsersByEmail(@Param("email") String email);
+
+    public Long countById(Integer id);
+
+    @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+    @Modifying
+    public void updateEnabledStatus(Integer id, boolean enabled);
 }
