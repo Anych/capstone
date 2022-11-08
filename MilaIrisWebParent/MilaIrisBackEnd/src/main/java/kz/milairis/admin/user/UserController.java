0b5d5ed1484130d249a6e5090a3f41804440f671
Anych <anuar123@mail.ru>
2022-11-08 07:30:44 +0600

    Add export users csv file logic

--- MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/user/UserController.java
+++ MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/user/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -153,5 +154,12 @@ public class UserController {
         redirectAttributes.addFlashAttribute("message", message);
 
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/export/csv")
+    public void exportToCsv(HttpServletResponse response) throws IOException {
+        List<User> listUsers = service.listAll();
+        UserCsvExporter exporter = new UserCsvExporter();
+        exporter.export(listUsers, response);
     }
 }
