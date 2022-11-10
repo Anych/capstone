49234d6f03365aeb71ceb075c609fffffe0dcba8
Anych <anuar123@mail.ru>
2022-11-10 08:01:48 +0600

    Create category form and list categories methods

--- MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/category/CategoryService.java
+++ MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/category/CategoryService.java
@@ -4,7 +4,9 @@ import kz.milairis.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryService {
@@ -13,6 +15,44 @@ public class CategoryService {
 
     public List<Category> listAll() {
         return (List<Category>) repo.findAll();
+    }
+
+    public List<Category> listCategoriesUsedInForm() {
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+
+        Iterable<Category> categoriesInDB = repo.findAll();
+
+        for (Category category : categoriesInDB) {
+            if (category.getParent() == null) {
+                categoriesUsedInForm.add(new Category(category.getName()));
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children) {
+                    String name = "--" + subCategory.getName();
+                    categoriesUsedInForm.add(new Category(name));
+
+                    listChildren(categoriesUsedInForm, subCategory, 1);
+                }
+            }
+        }
+
+        return categoriesUsedInForm;
+    }
+
+    private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+
+        for (Category subCategory : children) {
+            StringBuilder name = new StringBuilder();
+            name.append("--".repeat(Math.max(0, newSubLevel)));
+            name.append(subCategory.getName());
+
+            categoriesUsedInForm.add(new Category(name.toString()));
+
+            listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+        }
     }
 }
 
