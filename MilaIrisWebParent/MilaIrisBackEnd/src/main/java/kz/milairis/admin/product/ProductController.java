d5b24def6b6e77b89be52d48c35e972d7729c3a9
Anych <anuar123@mail.ru>
2022-11-15 11:15:57 +0600

    Create product overview form with categories of brands

--- MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/product/ProductController.java
+++ MilaIrisWebParent/MilaIrisBackEnd/src/main/java/kz/milairis/admin/product/ProductController.java
@@ -1,10 +1,13 @@
 package kz.milairis.admin.product;
 
+import kz.milairis.admin.brand.BrandService;
+import kz.milairis.common.entity.Brand;
 import kz.milairis.common.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -13,6 +16,8 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private BrandService brandService;
 
     @GetMapping("/products")
     public String listAll(Model model) {
@@ -21,5 +26,26 @@ public class ProductController {
         model.addAttribute("listProducts", listProducts);
 
         return "products/products";
+    }
+
+    @GetMapping("/products/new")
+    public String newProduct(Model model) {
+        List<Brand> listBrands = brandService.listAll();
+
+        Product product = new Product();
+        product.setEnabled(true);
+        product.setInStock(true);
+
+        model.addAttribute("product", product);
+        model.addAttribute("listBrands", listBrands);
+        model.addAttribute("pageTitle", "Create New Product");
+
+        return "products/product_form";
+    }
+
+    @PostMapping("/products/save")
+    public String saveProduct(Product product) {
+
+        return "redirect:/products";
     }
 }
