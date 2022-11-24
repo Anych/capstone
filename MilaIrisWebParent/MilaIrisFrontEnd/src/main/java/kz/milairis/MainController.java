fe81f25fd097749a1da6c415aa2c2972c83aebb7
Anych <anuar123@mail.ru>
2022-11-24 14:37:43 +0600

    Create login page

--- MilaIrisWebParent/MilaIrisFrontEnd/src/main/java/kz/milairis/MainController.java
+++ MilaIrisWebParent/MilaIrisFrontEnd/src/main/java/kz/milairis/MainController.java
@@ -3,6 +3,9 @@ package kz.milairis;
 import kz.milairis.category.CategoryService;
 import kz.milairis.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,17 @@ public class MainController {
 	public String viewHomePage(Model model) {
 		List<Category> listCategories = categoryService.listNoChildrenCategories();
 		model.addAttribute("listCategories", listCategories);
+
 		return "index";
+	}
+
+	@GetMapping("/login")
+	public String viewLoginPage() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+
+		return "redirect:/";
 	}
 }
